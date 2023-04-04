4fd1b6138c4399993859091f079ad7a99479adc0
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-04-04 15:45:00 -0400

    create logout button

--- src/layouts/Aside.tsx
+++ src/layouts/Aside.tsx
@@ -1,4 +1,9 @@
-import { Box, Drawer, Toolbar } from "@mui/material";
+import {
+    Box,
+    Drawer,
+    Toolbar
+} from "@mui/material";
+import LogoutButton from "../components/Buttons/Logout";
 
 const drawerWidth = '350px';
 
@@ -28,10 +33,13 @@ const Sidebar = () => (
             sx={{
                 height: '100%',
                 maxWidth: drawerWidth,
-                overflowY: 'auto'
+                overflowY: 'auto',
+                display: 'flex',
+                p: 2
             }}
             id="drawer-container"
         >
+            <LogoutButton />
         </Box>
     </Drawer>
 )
