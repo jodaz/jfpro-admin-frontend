8009c917cfa6bda40c3c3d43c374f62a796c0851
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-04-13 17:26:50 -0400

    finish layout screens

--- src/layouts/Chat/index.tsx
+++ src/layouts/Chat/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Grid, useMediaQuery, useTheme } from '@mui/material';
+import { Button, Grid, useMediaQuery, useTheme } from '@mui/material';
 import ChatList from './ChatList';
 import { LayoutProps } from '../../types';
 import ToggleButtonGroup from '../../components/ToggleButtonGroup';
@@ -7,6 +7,8 @@ import ToggleButton from '../../components/ToggleButton';
 import { useAuth } from '../../providers/AuthContext';
 import PrivateRoute from '../../components/PrivateRoute';
 import { useParams } from 'react-router-dom';
+import LinkBehavior from '../../components/LinkBehavior';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 interface ChatLayoutProps extends LayoutProps {
     chats: any,
@@ -21,6 +23,25 @@ const Chat: React.FC<ChatLayoutProps> = ({
     const theme = useTheme()
     const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
     const { state: { user } } = useAuth()
+    const { chat_id } = useParams()
+
+    const renderSmallScreens = () => (
+        <Grid container sx={{
+            minHeight: { xs: '75vh', sm: '55vh', md: '60vh', lg: '70vh' },
+            width: '100%'
+        }}>
+            {!!(!chat_id ) && (
+                <Grid item md={4} sm={12} xs={12} height='inherit'>
+                    <ChatList chats={chats} route={route} />
+                </Grid>
+            )}
+            {!!(chat_id ) && (
+                <Grid item md={8}>
+                    {children}
+                </Grid>
+            )}
+        </Grid>
+    )
 
     if (!user) return null;
 
@@ -28,34 +49,49 @@ const Chat: React.FC<ChatLayoutProps> = ({
         <Grid container rowSpacing={2} direction="column" sx={{
             paddingTop: isSmall ? '6rem' : 'unset'
         }}>
-            <PrivateRoute authorize='administrador'>
+            {!(isSmall && chat_id ) ? (
+                <PrivateRoute authorize='administrador'>
+                    <Grid item sm padding='0 !important' mb={1}>
+                        <ToggleButtonGroup>
+                            <ToggleButton value="/chat/premium">
+                                Premium
+                            </ToggleButton>
+                            <ToggleButton value="/chat/channels">
+                                Free
+                            </ToggleButton>
+                        </ToggleButtonGroup>
+                    </Grid>
+                </PrivateRoute>
+            ) : (
                 <Grid item sm padding='0 !important' mb={1}>
-                    <ToggleButtonGroup>
-                        <ToggleButton value="/chat/premium">
-                            Premium
-                        </ToggleButton>
-                        <ToggleButton value="/chat/channels">
-                            Free
-                        </ToggleButton>
-                    </ToggleButtonGroup>
+                    <Button
+                        component={LinkBehavior}
+                        to='/chat/channels'
+                        color='primary'
+                        startIcon={<ArrowBackIosIcon />}
+                    >
+                        Regresar
+                    </Button>
                 </Grid>
-            </PrivateRoute>
+            )}
             <Grid item sm={12} sx={{
                 border: '1px solid #DDE2E8',
                 borderRadius: '12px',
                 paddingTop: '0 !important'
             }}>
-                <Grid container sx={{
-                    minHeight: { xs: '90vh', sm: '55vh', md: '60vh', lg: '70vh' },
-                    width: '100%'
-                }}>
-                    <Grid item md={4} sm={12} xs={12} height='inherit'>
-                        <ChatList chats={chats} route={route} />
+                {isSmall ? (<>{renderSmallScreens()}</>) : (
+                    <Grid container sx={{
+                        minHeight: { xs: '80vh', sm: '55vh', md: '60vh', lg: '70vh' },
+                        width: '100%'
+                    }}>
+                        <Grid item md={4} sm={12} xs={12} height='inherit'>
+                            <ChatList chats={chats} route={route} />
+                        </Grid>
+                        <Grid item md={8}>
+                            {children}
+                        </Grid>
                     </Grid>
-                    <Grid item md={8}>
-                        {children}
-                    </Grid>
-                </Grid>
+                )}
             </Grid>
         </Grid>
     );
