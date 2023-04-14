150569db06a21d29ed13666b93272ba1cae8d049
Jesus Ordosgoitty <jesus@alaxatech.com>
2023-04-14 10:32:10 -0400

    fix bug: plan not found

--- src/layouts/Chat/ChatCard.tsx
+++ src/layouts/Chat/ChatCard.tsx
@@ -112,12 +112,14 @@ const ChatCard: React.FC<ChatCardProps> = ({
                             <Typography variant="subtitle1" fontWeight={500}>
                                 {truncateString(`${data.user.name} ${data.user.lastName ? data.user.lastName : ''}`, 12 )}
                             </Typography>
-                            <Chip
-                                label={data.plan.name}
-                                color="success"
-                                size='small'
-                                sx={{ fontWeight: 500, color: '#fff', ml: 1 }}
-                            />
+                            {data.plan && (
+                                <Chip
+                                    label={data.plan.name}
+                                    color="success"
+                                    size='small'
+                                    sx={{ fontWeight: 500, color: '#fff', ml: 1 }}
+                                />
+                            )}
                         </>
                     )}
                 </Box>
